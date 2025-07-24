af15dd94d2cc60c0f87e4707c0a8ac92f3ffb021
Raghu K <raghuk.vit@gmail.com>
2025-07-24 06:34:57 +0530

    RoboShop Project

--- infra-helm-charts/main.tf
+++ infra-helm-charts/main.tf
@@ -114,3 +114,14 @@ resource "helm_release" "prometheus" {
   ]
 }
 
+# Direct Helm Chart is a Problem - https://github.com/kubernetes/ingress-nginx/issues/10863
+
+resource "null_resource" "nginx-ingress" {
+  depends_on = [null_resource.kubeconfig]
+  provisioner "local-exec" {
+    command = <<EOF
+ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
+EOF
+  }
+}
+
