05934bf9e238963cdf587f888422974dcae5bb8b
Raghu K <raghuk.vit@gmail.com>
2025-07-24 07:17:44 +0530

    RoboShop Project

--- infra-helm-charts/main.tf
+++ infra-helm-charts/main.tf
@@ -64,9 +64,22 @@ TF
   }
 }
 
+# Direct Helm Chart is a Problem - https://github.com/kubernetes/ingress-nginx/issues/10863
+
+resource "null_resource" "nginx-ingress" {
+  depends_on = [null_resource.kubeconfig]
+  provisioner "local-exec" {
+    command = <<EOF
+ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
+EOF
+  }
+}
+
+
 resource "helm_release" "argocd" {
   depends_on = [
-    null_resource.kubeconfig
+    null_resource.kubeconfig,
+    null_resource.nginx-ingress
   ]
 
   name             = "argo-cd"
@@ -88,7 +101,10 @@ resource "helm_release" "argocd" {
 ## Filebeat Helm Chart
 resource "helm_release" "filebeat" {
 
-  depends_on = [null_resource.kubeconfig]
+  depends_on = [
+    null_resource.kubeconfig,
+    null_resource.nginx-ingress
+  ]
   name       = "filebeat"
   repository = "https://helm.elastic.co"
   chart      = "filebeat"
@@ -123,14 +139,4 @@ resource "helm_release" "prometheus" {
   ]
 }
 
-# Direct Helm Chart is a Problem - https://github.com/kubernetes/ingress-nginx/issues/10863
-
-resource "null_resource" "nginx-ingress" {
-  depends_on = [null_resource.kubeconfig]
-  provisioner "local-exec" {
-    command = <<EOF
- kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
-EOF
-  }
-}
 
